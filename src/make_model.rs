0055b5e19ff717e385ec5e2cf1e48d0c374bfe8b
ippee <ippee.music@gmail.com>
2021-11-02 13:48:20 +0900

    Move make_model function into model module

--- src/make_model.rs
+++
@@ -1,3 +0,0 @@
-use crate::model::LyrianModel;
-
-pub fn make_model(contents: &str) -> Result<Vec<LyrianModel>, String> {}
--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,11 @@
 use crate::markov::MarkovState;
+use crate::morphological_analysis::tokenize;
 
 pub struct LyrianModel {
-    word: String,
-    mora: String,
-    syllable: String,
-    state_space: Vec<MarkovState>,
+    pub word: String,
+    pub mora: String,
+    pub syllable: String,
+    pub state_space: Vec<MarkovState>,
 }
+
+pub fn make_model(contents: &str) -> Result<Vec<LyrianModel>, String> {}

e5f3f6afdcdfb66b2b8b33fd028a5ad44118acd3
ippee <ippee.music@gmail.com>
2021-11-03 16:28:05 +0900

    Implement process of generate_lyric temporarily

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,7 @@
 use crate::markov::MarkovModel;
 use crate::morphological_analysis::tokenize;
 use serde::{Deserialize, Serialize};
+use std::mem;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct LyrianModel {
@@ -31,19 +32,46 @@ impl<'a> LyrianModel {
         }
     }
 
-    pub fn generate_lyrics(
+    pub fn generate_lyric(
         &self,
-        phrase_len: usize,
+        lyric_len: usize,
         first_word_len: usize,
         rhythmical: bool,
     ) -> Result<String, String> {
-        if phrase_len < first_word_len {
+        if lyric_len < first_word_len {
             return Err(String::from(
                 "The phrase length must be longer than the first word length.",
             ));
         }
 
-        Ok(String::from(""))
+        for _ in 0..64 {
+            let first_token = &self.markov.get_random_state(first_word_len, rhythmical);
+            let chained_tokens = Vec::new();
+
+            for _ in 0..64 {
+                chained_tokens.push(first_token.get_random_token(rhythmical));
+                let generated_len = {
+                    let first_token_len = first_token.token.length(rhythmical);
+                    let chained_tokens_len = chained_tokens
+                        .iter()
+                        .fold(0, |acc, cur| acc + cur.length(rhythmical));
+                    first_token_len + chained_tokens_len
+                };
+
+                if lyric_len < generated_len {
+                    mem::drop(chained_tokens);
+                }
+
+                if lyric_len == generated_len {
+                    let chained_words = chained_tokens.iter().fold("", |acc, cur| acc + cur.word);
+                    return Ok(first_token.token.word + chained_words);
+                }
+            }
+        }
+
+        Err(String::from(
+            "Could not generate a lyric in given arguments.",
+        ))
     }
 
     pub fn to_json_str(&self) -> Result<String, String> {

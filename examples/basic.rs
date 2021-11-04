f689468cedcc47b4b8490bd0474bbd79cae0d485
ippee <ippee.music@gmail.com>
2021-11-04 19:32:05 +0900

    Update examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,11 +2,28 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
+    // Read learning data.
     let mut f = File::open("./examples/sample_text.txt").unwrap();
     let mut contents = String::new();
     f.read_to_string(&mut contents).unwrap();
 
+    // Make model from text data.
     let mut model = lyrian::model::LyrianModel::from_str(&*contents).unwrap();
 
-    println!("{:?}", model.generate_lyric(7, true).unwrap());
+    // Generate rhythmical lyric.
+    // If the argument "rhythmical" is true, the lyric will be generated on a syllable unit.
+    // Also, the voiceless sound will be taken into account.
+    let lyric = model.generate_lyric(7, true).unwrap();
+    println!("{}", lyric.join());
+
+    // Convert model to json.
+    let json = model.to_json_str().unwrap();
+
+    // Make model from json.
+    let mut model = lyrian::model::LyrianModel::from_json(&*json).unwrap();
+
+    // Generate unrhythmical lyric.
+    // If the argument "rhythmical" is false, the lyric will be generated on a mora unit.
+    let lyric = model.generate_lyric(10, false).unwrap();
+    println!("{}", lyric.join());
 }

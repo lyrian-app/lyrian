d6381593b89bb03ae1077b695fe332732d149a55
ippee <ippee.music@gmail.com>
2021-11-05 22:56:40 +0900

    Add document of lyric module

--- src/lyric.rs
+++ src/lyric.rs
@@ -1,24 +1,29 @@
 use crate::morphological_analysis::LyrianToken;
 
+/// Lyric structure
 pub struct Lyric {
     tokens: Vec<LyrianToken>,
 }
 
 impl Lyric {
+    /// Creates a new instance of `Lyric`.
     pub fn new(tokens: Vec<LyrianToken>) -> Lyric {
         Lyric { tokens: tokens }
     }
 
+    /// Returns the length of the generated lyric.
     pub fn length(&self, syllable: bool) -> usize {
         self.tokens
             .iter()
             .fold(0, |acc, cur| acc + cur.length(syllable))
     }
 
+    /// Adds a new `LyrianToken` to `Lyric`.
     pub fn add_token(&mut self, token: LyrianToken) {
         self.tokens.push(token);
     }
 
+    /// Join the words of `Vec<LyrianToken>`.
     pub fn join(&self) -> String {
         self.tokens
             .iter()

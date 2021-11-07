e52fbe65e37dc0f262e956893f976668531daff6
ichi-h <ippee.music@gmail.com>
2021-11-07 17:22:22 +0900

    Remove unnecessary public modifier

--- src/lib.rs
+++ src/lib.rs
@@ -43,9 +43,10 @@
 //! ```
 //!
 
-pub mod chars;
 pub mod lyric;
-pub mod markov;
 pub mod model;
-pub mod morphological_analysis;
-pub mod walkers_alias_method;
+
+mod chars;
+mod markov;
+mod morphological_analysis;
+mod walkers_alias_method;

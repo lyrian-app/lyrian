7e6f20b439f3afa2b63963dab2da8570d9c54f2d
ichi-h <ippee.music@gmail.com>
2021-11-07 17:39:46 +0900

    Fix examples

--- examples/from_json.rs
+++ examples/from_json.rs
@@ -4,21 +4,17 @@ use std::io::prelude::*;
 use lyrian::model::LyrianModel;
 
 fn main() {
-    // Read learning data.
-    let mut f = File::open("./examples/sample_text.txt").unwrap();
+    // Read json file.
+    let mut f = File::open("./examples/sample_model.json").unwrap();
     let mut contents = String::new();
     f.read_to_string(&mut contents).unwrap();
 
-    // Build model from text data.
-    let mut model = LyrianModel::from_str(&*contents).unwrap();
+    // Build model from json.
+    let mut model = LyrianModel::from_json(&*contents).unwrap();
 
     // Generate lyric.
     let lyric_1 = model.generate_lyric(7, true).unwrap();
     let lyric_2 = model.generate_lyric(7, false).unwrap();
     println!("syllable: {}", lyric_1.join());
     println!("mora    : {}", lyric_2.join());
-
-    // Write json file.
-    // let mut f_json = File::create("./examples/sample_model2.json").unwrap();
-    // f_json.write_all(model.to_json_str().unwrap().as_bytes()).unwrap();
 }
